c49a6036d1ea04db79ed80809f5e159cb202db2e
mkane2 <self.striping.sock@gmail.com>
2020-01-22 21:07:47 -0500

    update confetti

--- app/helpers/attempt_choices_helper.rb
+++ app/helpers/attempt_choices_helper.rb
@@ -1,6 +1,6 @@
 module AttemptChoicesHelper
   def correct_confetti
-    confetti = ["confetti.gif", "egg.gif", "excellent.gif", "good.gif", "shark.gif", "sparkle.gif", "wow.gif", "yay.gif", "yes.gif"]
+    confetti = ["confetti.gif", "egg.gif", "excellent.gif", "good.gif", "nice.gif", "shark.gif", "sparkle.gif", "stars.gif", "wow.gif", "wow2.gif", "yay.gif", "yes.gif"]
     this_one = confetti.sample
   end
 end
