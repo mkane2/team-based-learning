7eaa9f9fa61f717cf12728fa40f0e58d51e9e534
mkane2 <self.striping.sock@gmail.com>
2020-01-24 12:36:29 -0500

    Update timezone

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module Tbl
     config.school_email_domain = "albany.edu" # The email domain that your users must use.
     config.site_name = "Awesome TBL Course" # What gets displayed on the browser tab
     config.default = "defaultpassword" # This will be your default password when the app is created.  Make sure you change your password in /users/edit when you log in the first time, and make sure your students change their emails as well!
+
+    config.time_zone = 'Eastern Time (US & Canada)' #Set your time zone
   end
 end
