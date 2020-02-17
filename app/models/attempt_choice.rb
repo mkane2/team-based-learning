55a929265d1b4b43f2eeeabcd873b38b60cdf598
mkane2 <self.striping.sock@gmail.com>
2020-02-16 21:18:33 -0500

    Validate individual and team attempts

--- app/models/attempt_choice.rb
+++ app/models/attempt_choice.rb
@@ -4,5 +4,11 @@ class AttemptChoice < ApplicationRecord
   belongs_to :choice
   belongs_to :user
   belongs_to :team, optional: true
-  validates_uniqueness_of :choice, scope: %i[user_id team_id]
+  #validates_uniqueness_of :choice, scope: %i[user_id team_id]
+  validates_uniqueness_of :choice, scope: %i[team_id attempt_id user_id]
+  validates_uniqueness_of :question, scope: %i[attempt_id user_id], if: :individual_attempt?
+
+  def individual_attempt?
+    team_id == nil
+  end
 end
